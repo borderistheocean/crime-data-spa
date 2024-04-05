2da052c514d7aa179638f0db3ef11cbce0973e33
borderistheocean <68243063+borderistheocean@users.noreply.github.com>
2024-04-05 18:58:36 +0100

    Added router with code for handling search parameter string opertaions, refactored whitespace trimming

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,86 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
-import { postcodeValidator, postcodeValidatorExistsForCountry } from "postcode-validator";
+import { postcodeValidator } from "postcode-validator";
+import { useSearchParams } from "react-router-dom";
 
 function App() {
   const locale = "GB";
   const [postcodeInputValue, setpostcodeInputValue] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const getPostCodeData = (searchQuery: string) => {
-    searchQuery.split(",").forEach((postcode, index) => {
-      const trimPostcode = postcode.trim();
+  const stripWhitespace = (postcode: string) => {
+    return postcode.replace(/ /g, '');
+  };
 
-      if (postcodeValidatorExistsForCountry(locale) && postcodeValidator(trimPostcode, locale)) {
-        setTimeout(() => {
-          axios.get(`http://api.getthedata.com/postcode/${trimPostcode}`)
-            .then(function (response) {
-              if (response.data.status === "match" && response.data.data.latitude && response.data.data.longitude) {
-                axios.post(`https://data.police.uk/api/crimes-street/all-crime?lat=${response.data.data.latitude}&lng=${response.data.data.longitude}`)
-                  .then(function (response) {
-                    // Reponse for street crime query
-                    console.log(response);
-
-                    if (response.data) {
-                      response.data.forEach((crimeData: any, index: number) => {
-                        console.log(`<<< result ${index}`)
-                        // Category
-                        console.log(crimeData.category)
-                        // Postcode
-                        console.log(postcode)
-                        // Date of crime
-                        console.log(crimeData.month)
-                        // Approximate street name
-                        console.log(crimeData.location.street.name)
-                        // Outcome status
-                        console.log(crimeData.outcome_status.category)
-                        console.log(">>>")
-                      });
-                    }
-                  })
-                  .catch(function (error) {
-                    console.log(error);
-                  })
-              }
-            })
-            .catch(function (error) {
-              console.log(error);
-            })
-        }, index * 1000);
+  const arePostcodesValid = (searchQuery: string) => {
+    let resultValid = true;
+    searchQuery && searchQuery.split(",").forEach((postcode) => {
+      if (!postcodeValidator(stripWhitespace(postcode), locale)) {
+        resultValid = false;
       }
     });
+    return resultValid;
   }
 
+  const getPostCodeData = (searchQuery: string) => {
+    if (arePostcodesValid(searchQuery)) {
+      setSearchParams({ "postcode": searchQuery });
+
+      searchQuery.split(",").forEach((postcode, index) => {
+        const trimmedPostcode = stripWhitespace(postcode);
+
+        if (postcodeValidator(trimmedPostcode, locale)) {
+          setTimeout(() => {
+            axios.get(`http://api.getthedata.com/postcode/${trimmedPostcode}`)
+              .then(function (response) {
+                if (response.data.status === "match" && response.data.data.latitude && response.data.data.longitude) {
+                  axios.post(`https://data.police.uk/api/crimes-street/all-crime?lat=${response.data.data.latitude}&lng=${response.data.data.longitude}`)
+                    .then(function (response) {
+                      // Reponse for street crime query
+                      console.log(response);
+
+                      if (response.data) {
+                        response.data.forEach((crimeData: any, index: number) => {
+                          console.log(`<<< result ${index}`)
+                          // Category
+                          console.log(crimeData.category)
+                          // Postcode
+                          console.log(postcode)
+                          // Date of crime
+                          console.log(crimeData.month)
+                          // Approximate street name
+                          console.log(crimeData.location.street.name)
+                          // Outcome status
+                          console.log(crimeData.outcome_status.category)
+                          console.log(">>>")
+                        });
+                      }
+                    })
+                    .catch(function (error) {
+                      console.log(error);
+                    })
+                }
+              })
+              .catch(function (error) {
+                console.log(error);
+              })
+          }, index * 1000);
+        }
+      });
+    }
+  }
+
+  useEffect(() => {
+    const postcodeParameter = searchParams.get('postcode') as string;
+    if (postcodeParameter && arePostcodesValid(postcodeParameter)) {
+      setpostcodeInputValue(postcodeParameter);
+      getPostCodeData(postcodeParameter);
+    }
+  }, []);
+
   return (
-    <div className="App">
+    <div>
       <label htmlFor="postcodeInput">Postcode/s: </label>
       <input id="postcodeInput" placeholder="" value={postcodeInputValue} onChange={e => setpostcodeInputValue(e.target.value)} />
       <button onClick={() => getPostCodeData(postcodeInputValue)}>Search</button>
