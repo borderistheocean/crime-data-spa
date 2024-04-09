4050771743ea834a79adca8e8c50be69e10f8f76
borderistheocean <68243063+borderistheocean@users.noreply.github.com>
2024-04-09 17:56:40 +0100

    Added navigation and table rendering, lodash used for grouping crime data entries by type

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import "./App.css";
 import axios from "axios";
 import { postcodeValidator } from "postcode-validator";
 import { useSearchParams } from "react-router-dom";
+import _ from "lodash";
 
 function App() {
   const locale = "GB";
   const [postcodeInputValue, setpostcodeInputValue] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
+  const [crimesList, setCrimesList] = useState([] as any);
   const previousInputValue = useRef("");
 
   const stripWhitespace = (postcode: string) => {
@@ -32,36 +34,33 @@ function App() {
     }
 
     if (arePostcodesValid(searchQuery)) {
+      setCrimesList([]);
       searchQuery && searchQuery.split(",").forEach((postcode, index) => {
         const trimmedPostcode = stripWhitespace(postcode);
+
         if (postcodeValidator(trimmedPostcode, locale)) {
           setTimeout(() => {
             axios.get(`http://api.getthedata.com/postcode/${trimmedPostcode}`)
               .then(function (response) {
+
                 if (response.data.status === "match" && response.data.data.latitude && response.data.data.longitude) {
                   axios.post(`https://data.police.uk/api/crimes-street/all-crime?lat=${response.data.data.latitude}&lng=${response.data.data.longitude}`)
                     .then(function (response) {
                       // Reponse for street crime query
                       console.log(response);
+
                       if (response.data) {
-                        response.data.forEach((crimeData: any, index: number) => {
-                          console.log(`<<< result ${index}`)
-                          // Category
-                          console.log(crimeData.category)
-                          // Postcode
-                          console.log(postcode)
-                          // Date of crime
-                          console.log(crimeData.month)
-                          // Approximate street name
-                          console.log(crimeData.location.street.name)
-                          // Outcome status
-                          if (crimeData.outcome_status) {
-                            console.log(crimeData.outcome_status.category)
-                          } else {
-                            console.log("On going");
-                          }
-                          console.log(">>>")
+                        response.data.forEach((crimeData: any) => {
+                          crimeData.postcode = trimmedPostcode;
                         });
+
+                        if (response.data.length > 0) {
+                          // Group elements of crimes by crime type           
+                          // # TODO: remove category property from within entries
+                          // # TODO: merge crime data arrays with every subsequent data fetch          
+                          const chained = _.chain(response.data).groupBy("category").map((value, key) => ({ type: key, entries: value })).value();
+                          setCrimesList((crimesList: any) => [...crimesList, { "data": chained }]);
+                        }
                       }
                     })
                     .catch(function (error) {
@@ -83,11 +82,47 @@ function App() {
     }
   }, [searchParams]);
 
+  const crimeNavigation = crimesList.map((crimeData: any, index: number) =>
+    <ul key={index.toString()}>
+      {crimeData.data.map((c: any, i: any) => (
+        <li key={i}><a href={`#${c.type}`}>{c.type}</a></li>
+      ))}
+    </ul>
+  );
+
+  const crimeTables = crimesList.map((crimeData: any, index: number) =>
+    <>
+      {crimeData.data.map((c: any, i: any) => (
+        <>
+          <h1 id={c.type}>{c.type}</h1>
+          <table>
+            <tr>
+              <th>Postcode</th>
+              <th>Month</th>
+              <th>Street</th>
+              <th>Status</th>
+            </tr>
+            {c.entries.map((d: any, ix: any) => (
+              <tr>
+                <td>{d.postcode}</td>
+                <td>{d.month}</td>
+                <td>{d.location.street.name}</td>
+                <td>{(d.outcome_status) ? d.outcome_status.category : "ongoing"}</td>
+              </tr>
+            ))}
+          </table>
+        </>
+      ))}
+    </>
+  );
+
   return (
     <div>
       <label htmlFor="postcodeInput">Postcode/s: </label>
       <input id="postcodeInput" placeholder="" value={postcodeInputValue} onChange={e => setpostcodeInputValue(e.target.value)} />
       <button onClick={() => setSearchParams({ "postcode": postcodeInputValue })}>Search</button>
+      {crimeNavigation}
+      {crimeTables}
     </div>
   );
 }
