db35ef651ac86e5f3e260eba7cf351bd916ed46a
borderistheocean <68243063+borderistheocean@users.noreply.github.com>
2024-06-12 16:38:24 +0100

    added new test user events for postcode input

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,11 @@
-import { render, screen, waitFor } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import App from './App';
 import { BrowserRouter, MemoryRouter } from 'react-router-dom'
 import { mockMatchMedia } from './setupTests';
 
 // https://testing-library.com/docs/example-react-router/
+// https://testing-library.com/docs/example-input-event/
 
 beforeAll(() => {
   mockMatchMedia();
@@ -13,20 +14,44 @@ beforeAll(() => {
 test('Render initial screen without a parameter', async () => {
   render(<App />, { wrapper: BrowserRouter })
 
-  // verify page content for default route
+  // Verify page content for default route
   expect(screen.getByText(/enter postcode\(s\) to search for crimes, entries such as b46qb and le11aa usually yield good amounts of crime data\./i)).toBeInTheDocument();
 });
 
 test('Render initial screen without a parameter then search for a postcode', async () => {
   render(<App />, { wrapper: BrowserRouter })
+  
   // verify page content for default route
   expect(screen.getByText(/enter postcode\(s\) to search for crimes, entries such as b46qb and le11aa usually yield good amounts of crime data\./i)).toBeInTheDocument();
 
-  // https://testing-library.com/docs/example-input-event/
+  // Enter new postcode value
+  fireEvent.change(screen.getByRole('textbox'), { target: { value: "LE11AA" } });
+  // Click submit
+  fireEvent.click(screen.getByRole('button', { name: /search/i }));
 
-  // screen.getByRole('textbox')
+  // Check that new entry exists in the history component 
 
-  // userEvent.
+  // TODO
+
+  // Check headings are rendered
+  await waitFor(() =>
+    expect(screen.getByRole('heading', {
+      name: /anti social behaviour/i
+    })).toBeInTheDocument()
+  );
+
+  // Check navigation headings are rendered
+  await waitFor(() =>
+    expect(screen.getByRole('link', {
+      name: /anti social behaviour/i
+    })).toBeInTheDocument()
+  );
+
+  // Check history entry can be removed
+  fireEvent.click(screen.getByText(/remove/i));
+  
+  // Check that history is clear
+  screen.debug();
 });
 
 test('Render screen with postcode parameter', async () => {
@@ -36,11 +61,9 @@ test('Render screen with postcode parameter', async () => {
     </MemoryRouter>,
   );
 
-  // Expect titles to show on render
   await waitFor(() =>
     expect(getByRole('heading', {
       name: /anti social behaviour/i
     })).toBeInTheDocument()
   );
-
 });
